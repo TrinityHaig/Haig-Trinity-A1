ba8c70555b6c338a4397a03d9ac9675fd5a9b7ed
TrinityHaig <000915272@mohawkcollege.ca>
2025-01-16 11:34:08 -0500

    update 2

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -5,23 +5,46 @@
         static void Main(string[] args)
         {
             {
-                Console.WriteLine("Welcome! Todays menu item is [CAKE]. Would you like to try and make one?");
+                //make the answers not be case sensitive (i think i need to use a bool parse which means id need to convert them to bools)
+
+                Console.WriteLine("Welcome! Todays menu item is [CAKE]. Would you like to try and make one?\n\nYes\nNo");
                 string choice1 = Console.ReadLine();
                 //if the cake has any errors it will give you a fail screen at the end.
                 bool doesCakeHaveMistakes = false;
                 //end if they type no
                 if (choice1 == "yes")
                 {
-                    Console.WriteLine("Great! Pick from the following flavors!");
-                    Console.WriteLine("Strawberry Cake");
+                    Console.WriteLine("\nGreat! Pick from the following flavors!\n");
+                    Console.WriteLine("Strawberry");
                     Console.WriteLine("Cheesecake");
-                    Console.WriteLine("Ice cream cake\n");
+                    Console.WriteLine("Ice cream\n");
                     string cakeFlavour = Console.ReadLine();
                     //strawberry cake path
                     if (cakeFlavour == "strawberry")
                     {
-                        Console.WriteLine("You have chosen", cakeFlavour);
-                        Console.WriteLine("Which of the following ingredients is needed for the recipe?\n");
+                        Console.WriteLine("\nYou have chosen");
+                        Console.WriteLine(cakeFlavour);
+                        Console.WriteLine("Which of the following ingredients is NOT needed for the recipe?\n");
+                        //Design the cake in this scenario i dont have it planned out just yet
+                    }
+                    else if (cakeFlavour == "cheesecake")
+                    {
+                        Console.WriteLine("\nYou have chosen");
+                        Console.WriteLine(cakeFlavour);
+                        Console.WriteLine("Which of the following ingredients is NOT needed for the recipe?\n");
+                    }
+
+                    else if (cakeFlavour == "ice cream")
+                    {
+                        Console.WriteLine("\nYou have chosen");
+                        Console.WriteLine(cakeFlavour);
+                        Console.WriteLine("Which of the following ingredients is NOT needed for the recipe?\n");
+                    }
+
+                    else
+                    {
+                        //i really wish this didnt just instantly end it but thats the last priority currently
+                       Console.WriteLine("I did not understand the prompt, please try again later");
                     }
                 }
 
--- ConsoleApp3/obj/Debug/net8.0/ConsoleApp3.AssemblyInfo.cs
+++ ConsoleApp3/obj/Debug/net8.0/ConsoleApp3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e3c9af991ba4ba09bb017f186028b2161094e50")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
