4d2015e1b4f9c0d3588777d37c23027c023ab4d2
TrinityHaig <000915272@mohawkcollege.ca>
2025-01-16 10:13:04 -0500

    start

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Writeline("Welcome! Todays menu item is [CAKE]. Would you like to try and make one?");
+            string choice1 = Console.ReadLine();
+            //end if they type no
+            if (choice1 == "yes")
+            {
+                Console.WriteLine("Great! Pick from the following flavors!");
+                Console.WriteLine("Strawberry Cake");
+                Console.WriteLine("Cheesecake");
+                Console.WriteLine("Ice cream cake");
+            }
+
+            else if (choice1 == "no")
+            {
+                Console.Writeline("Very well, have a nice day");
+            }
+            //If possible send back to start but ill try to mess with that later its not my main priority
+            else
+            {
+                Console.WriteLine("I did not understand the prompt, please try again later");
+            }
+
+        }
+    }
+}
